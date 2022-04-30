1714210426ff8e6d12cb4405896a6a927f53b007
gefgu <gefgu@hotmail.com>
2022-04-30 16:21:05 -0300

    Add heading component with html elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from "./components/Header";
+
 const App = () => {
-  return <h1>Hello!</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>FakeStore</h1>
+      <nav>
+        <div>Home</div>
+        <div>Products</div>
+        <div>Cart</div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
