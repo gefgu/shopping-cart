2192890bcb9e92fbf7725f53ba4529217731ceac
gefgu <gefgu@hotmail.com>
2022-04-30 19:55:05 -0300

    Add homepage message text and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,12 @@ import "../styles/home.css";
 const Home = () => {
   return (
     <div className="home">
+      <div className="message">
+        <h3>BEST ONLINE STORE OF THE YEAR</h3>
+        <h2>Buy the best nature products</h2>
+        <button>Shop now</button>
+      </div>
+
       <div className="footer">
         Photo by{" "}
         <a href="https://unsplash.com/@masterwalter?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
