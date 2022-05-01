1fe6948b7cf32f5102eb77d7bbd48b21777df4aa
gefgu <gefgu@hotmail.com>
2022-05-01 09:56:13 -0300

    Add cosmere background photo with new collor pallete and remove footing

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,17 +11,6 @@ const Home = () => {
           <button>Shop now</button>
         </Link>
       </div>
-
-      <div className="footer">
-        Photo by{" "}
-        <a href="https://unsplash.com/@masterwalter?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-          Jan Walter Luigi
-        </a>{" "}
-        on{" "}
-        <a href="https://unsplash.com/t/wallpapers?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-          Unsplash
-        </a>
-      </div>
     </div>
   );
 };
