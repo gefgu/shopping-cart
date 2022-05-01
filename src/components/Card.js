ebeb639f9f390ad7187d850ddf4971c332018f19
gefgu <gefgu@hotmail.com>
2022-05-01 10:30:37 -0300

    Add images to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 const Card = ({ product }) => {
   return (
-    <div>
+    <div className="card">
+      <img alt={product.name} src={require(`../assets/${product.fileName}`)} />
       <p>{product.name}</p>
     </div>
   );
