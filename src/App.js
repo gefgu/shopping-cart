5532e4fd66d63386b5d581637744c736262d1302
gefgu <gefgu@hotmail.com>
2022-05-01 10:17:56 -0300

    Add product list in state linking to filenames

--- src/App.js
+++ src/App.js
@@ -2,8 +2,33 @@ import "./styles/app.css";
 import Header from "./components/Header";
 import RouteSwtich from "./RouteSwitch";
 import { BrowserRouter } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const makeProduct = (productName) => {
+  const getFileName = (name) => {
+    return `${name.split(" ").join("_").toLowerCase()}.png`;
+  };
+
+  return {
+    name: productName,
+    fileName: getFileName(productName),
+  };
+};
 
 const App = () => {
+  const [productList, setProductList] = useState([
+    makeProduct("Bridge 4 Baseball Cap"),
+    makeProduct("Bridge 4 Patch"),
+    makeProduct("Bridge 4 T Shirt"),
+    makeProduct("Final Empire Map Poster"),
+    makeProduct("Journey Before Destination T Shirt"),
+    makeProduct("Life Before Death T Shirt"),
+    makeProduct("Roshar Map Poster"),
+    makeProduct("Strength Before Weakness T Shirt"),
+  ]);
+
+  console.log(productList);
+
   return (
     <div className="app">
       <BrowserRouter>
