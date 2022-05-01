b2378bd1fb6c2f198f32654fee460cff943b7373
gefgu <gefgu@hotmail.com>
2022-05-01 10:50:15 -0300

    Add product card styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,14 @@
+import "../styles/card.css";
+
 const Card = ({ product }) => {
   return (
     <div className="card">
       <img alt={product.name} src={require(`../assets/${product.fileName}`)} />
-      <p>{product.name}</p>
-      <p>${product.price}</p>
-      <button>Add to cart</button>
+      <div className="text">
+        <p className="product-name">{product.name}</p>
+        <p className="product-price">${product.price}</p>
+        <button className="add-to-cart">Add to cart</button>
+      </div>
     </div>
   );
 };
