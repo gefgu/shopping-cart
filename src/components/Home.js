73f243522afee20c7e06cbf3295019a4101fa665
gefgu <gefgu@hotmail.com>
2022-05-01 10:03:27 -0300

    Fix heading for cosmere theming

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="home">
       <div className="message">
         <h3>BEST ONLINE STORE OF THE YEAR</h3>
-        <h2>Buy the best nature products</h2>
+        <h2>Buy the best Cosmere products</h2>
         <Link to="products">
           <button>Shop now</button>
         </Link>
