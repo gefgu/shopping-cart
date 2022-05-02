aafb14a5803530e31d5d891e1339011eb23dc3b1
gefgu <gefgu@hotmail.com>
2022-05-02 20:10:42 -0300

    Add grid display styling of products in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,5 @@
 import ProductCartCounter from "./ProductCartCounter";
+import "../styles/cart.css";
 
 const Cart = ({ cartList, updateProductQuantityInCart }) => {
   const costTotal = cartList.reduce(
@@ -8,30 +9,33 @@ const Cart = ({ cartList, updateProductQuantityInCart }) => {
 
   return (
     <div>
-      {cartList.map((element) => {
-        const product = element.product;
-        const quantity = element.quantity;
+      <div className="cart-list">
+        {cartList.map((element) => {
+          const product = element.product;
+          const quantity = element.quantity;
 
-        return (
-          <div className="cart-item" key={product.id}>
-            <div className="left">
-              <img
-                alt={product.name}
-                src={require(`../assets/${product.fileName}`)}
-              />
-              <ProductCartCounter
-                product={product}
-                updateProductQuantityInCart={updateProductQuantityInCart}
-                cartList={cartList}
-              />
+          return (
+            <div className="cart-item" key={product.id}>
+              <div className="left">
+                <img
+                  alt={product.name}
+                  src={require(`../assets/${product.fileName}`)}
+                />
+                <ProductCartCounter
+                  product={product}
+                  updateProductQuantityInCart={updateProductQuantityInCart}
+                  cartList={cartList}
+                />
+              </div>
+              <div className="description">
+                <p className="product-name">{product.name}</p>
+                <p>Unit Price: ${product.price}</p>
+                <p>Total: ${product.price * quantity}</p>
+              </div>
             </div>
-            <div className="price-container">
-              <p>Unit Price: ${product.price}</p>
-              <p>Total: ${product.price * quantity}</p>
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       <p>Total Cost: ${costTotal}</p>
       <button>Checkout</button>
     </div>
