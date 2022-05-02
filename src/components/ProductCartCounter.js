0e0696710fd9c90d474ee524375859a40a88e232
gefgu <gefgu@hotmail.com>
2022-05-02 19:28:47 -0300

    Add counter styling

--- src/components/ProductCartCounter.js
+++ src/components/ProductCartCounter.js
@@ -1,3 +1,5 @@
+import "../styles/productCartCounter.css";
+
 const ProductCartCounter = ({
   product,
   updateProductQuantityInCart,
@@ -6,16 +8,23 @@ const ProductCartCounter = ({
   const productInCart = cartList.find((elem) => elem.product === product);
 
   return (
-    <div>
+    <div className="counter">
       <button
+        className="minus"
         onClick={() => {
           updateProductQuantityInCart(product, productInCart.quantity - 1);
         }}
       >
         -
       </button>
-      <input type="text" value={productInCart.quantity} readOnly />
+      <input
+        type="text"
+        value={productInCart.quantity}
+        readOnly
+        className="counter-value"
+      />
       <button
+        className="plus"
         onClick={() => {
           updateProductQuantityInCart(product, productInCart.quantity + 1);
         }}
